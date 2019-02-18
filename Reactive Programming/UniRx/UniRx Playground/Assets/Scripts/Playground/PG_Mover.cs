b5c6670cc7c7525c4b1ebbf72cabf75bf5214ed6
Sinisa Abramovic <sinisa.abramovich@gmail.com>
2019-02-18 19:56:52 +0100

    Reactive extension init setup

---
+++ Reactive Programming/UniRx/UniRx Playground/Assets/Scripts/Playground/PG_Mover.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using System;
+
+public class PG_Mover : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        ConformDoubleTap();
+    }
+
+    IEnumerator RoutineA()
+    {
+        Debug.Log("CALLED!");
+        yield return new WaitForSeconds(1);
+        MoveMe();
+    }
+
+    private void ConformDoubleTap()
+    {
+        var clickStream = Observable
+            .EveryUpdate()
+            .Where(_ => Input.GetMouseButtonDown(0));
+
+        clickStream
+            .Buffer(clickStream.Throttle(TimeSpan.FromMilliseconds(250)))
+            .Where(xs => xs.Count >= 2)
+            .Subscribe(xs => MoveInTime());
+    }
+
+    void MoveInTime()
+    {
+         var cancel = Observable.FromCoroutine(RoutineA)
+            .SelectMany(RoutineA)
+            .Subscribe();
+
+        cancel.Dispose();
+    }
+
+    private void MoveMe()
+    {
+        Debug.Log("MOVE");
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 0.5f, this.transform.position.z);
+    }
+}
