13de8d7c2a7d066a42021ce3145f4409b429300f
Sinisa Abramovic <sinisa.abramovich@gmail.com>
2019-02-19 19:50:06 +0100

    no message

--- Reactive Programming/UniRx/UniRx Playground/Assets/Scripts/Playground/PG_Mover.cs
+++ Reactive Programming/UniRx/UniRx Playground/Assets/Scripts/Playground/PG_Mover.cs
@@ -8,41 +8,135 @@ using System;
 public class PG_Mover : MonoBehaviour {
 
     // Use this for initialization
+    private Vector3 offset;
+    public GameObject mover;
+    public GameObject baseObject;
+
+    public GameObject up;
+    public GameObject down;
+    public GameObject left;
+    public GameObject right;
+
+    public int step = 4;
+
+    public float speed = 0.01f;
+
+    bool input = true;
+
     void Start () {
-        ConformDoubleTap();
+        //ConformDoubleTap();
+
+        //baseObject.transform.localRotation = Quaternion.Euler(-90, 0, 0);
     }
 
-    IEnumerator RoutineA()
+    private void Update()
     {
-        Debug.Log("CALLED!");
-        yield return new WaitForSeconds(1);
-        MoveMe();
+        if(input == true)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                input = false;
+                StartCoroutine(moveUp());
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                input = false;
+                StartCoroutine(moveDown());
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                input = false;
+                StartCoroutine(moveLeft());
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                input = false;
+                StartCoroutine(moveRight());
+            }
+        }
     }
 
-    private void ConformDoubleTap()
-    {
-        var clickStream = Observable
-            .EveryUpdate()
-            .Where(_ => Input.GetMouseButtonDown(0));
+    //IEnumerator RoutineA()
+    //{
+    //    Debug.Log("CALLED!");
+    //    yield return new WaitForSeconds(1);
+    //    MoveMe();
+    //}
 
-        clickStream
-            .Buffer(clickStream.Throttle(TimeSpan.FromMilliseconds(250)))
-            .Where(xs => xs.Count >= 2)
-            .Subscribe(xs => MoveInTime());
+    //private void ConformDoubleTap()
+    //{
+    //    var clickStream = Observable
+    //        .EveryUpdate()
+    //        .Where(_ => Input.GetMouseButtonDown(0));
+
+    //    clickStream
+    //        .Buffer(clickStream.Throttle(TimeSpan.FromMilliseconds(250)))
+    //        .Where(xs => xs.Count >= 2)
+    //        .Subscribe(xs => MoveInTime());
+    //}
+
+    //void MoveInTime()
+    //{
+    //     var cancel = Observable.FromCoroutine(RoutineA)
+    //        .SelectMany(RoutineA)
+    //        .Subscribe();
+
+    //    cancel.Dispose();
+    //}
+
+    //private void MoveMe()
+    //{
+    //    Debug.Log("MOVE");
+    //    this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 0.5f, this.transform.position.z);
+    //}
+
+    IEnumerator moveUp()
+    {
+
+
+        for (int i = 0; i < 90 / step; i++)
+        {
+            mover.transform.RotateAround(up.transform.position, Vector3.right, step);
+            yield return new WaitForSeconds(speed * Time.deltaTime);
+        }
+
+        baseObject.transform.position = mover.transform.position;
+        input = true;
     }
 
-    void MoveInTime()
+    IEnumerator moveDown()
     {
-         var cancel = Observable.FromCoroutine(RoutineA)
-            .SelectMany(RoutineA)
-            .Subscribe();
+        for (int i = 0; i < 90 / step; i++)
+        {
+            mover.transform.RotateAround(down.transform.position, Vector3.left, step);
+            yield return new WaitForSeconds(speed * Time.deltaTime);
+        }
 
-        cancel.Dispose();
+        baseObject.transform.position = mover.transform.position;
+        input = true;
     }
 
-    private void MoveMe()
+    IEnumerator moveLeft()
     {
-        Debug.Log("MOVE");
-        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 0.5f, this.transform.position.z);
+        for (int i = 0; i < 90 / step; i++)
+        {
+            mover.transform.RotateAround(left.transform.position, Vector3.forward, step);
+            yield return new WaitForSeconds(speed * Time.deltaTime);
+        }
+
+        baseObject.transform.position = mover.transform.position;
+        input = true;
+    }
+
+    IEnumerator moveRight()
+    {
+        for (int i = 0; i < 90 / step; i++)
+        {
+            mover.transform.RotateAround(right.transform.position, Vector3.back, step);
+            yield return new WaitForSeconds(speed * Time.deltaTime);
+        }
+
+        baseObject.transform.position = mover.transform.position;
+        input = true;
     }
 }
